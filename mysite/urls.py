291cb811c9d9f99f3a160d874542d38d11659b41
Aditya00993 <princebhargava240@gmail.com>
2024-06-06 19:38:50 +0530

    commit succesfully

---
+++ mysite/urls.py
@@ -0,0 +1,32 @@
+"""mysite URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from turtle import home
+from django.contrib import admin
+from django.urls import path
+from accounts.views import home, about,services,contact
+
+
+admin.site.site_header = "Aditysblog"
+admin.site.site_title = "Login"
+admin.site.index_title = "Welcome to Adityasblog"
+
+urlpatterns = [
+    path('',home),
+    path('about/',about),
+    path('admin/', admin.site.urls),
+    path('services',services),
+    path('contact',contact)
+]
